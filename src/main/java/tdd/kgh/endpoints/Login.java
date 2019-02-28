3cecf2fa3e06d0bbf20c8bb79d6223b882bf000c
Amey Deshpande <ameydeshpande30@gmail.com>
2019-03-01 02:22:03 +0530

    login refined

--- src/main/java/tdd/kgh/endpoints/Login.java
+++ src/main/java/tdd/kgh/endpoints/Login.java
@@ -2,17 +2,21 @@ package tdd.kgh.endpoints;
 
 
 
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("")
 public class Login {
 	@GetMapping("/login")
-	public String findAll(){
-		return "hello";
+	public HashMap<String, String> findAll(){
+		 HashMap<String, String> map = new HashMap<>();
+		    map.put("msg", "OK");
+		   
+		return map;
 	}
 
 }
